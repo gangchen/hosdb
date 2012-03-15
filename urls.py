f57dba5e11ee876a9cfe916824d076f85e498242
goodluckhym <764265099@qq.com>
2012-03-15 16:27:06 +0800

    change urls

--- urls.py
+++ urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-      url(r'^ostbase/', include('ostbase.url')),
+    # url(r'^ostbase/', include('ostbase.url')),
+      url(r'^$', include('ostbase.url')),
 )
