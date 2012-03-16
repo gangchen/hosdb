d072ef59fd39295e9fbe0aafaa3a26f63a8c95d4
goodluckhym <764265099@qq.com>
2012-03-16 13:38:55 +0800

    add search function

--- ostbase/views.py
+++ ostbase/views.py
@@ -7,8 +7,9 @@ from django.template import *
 from django.template.loader import get_template
 
 def genes(request):
-    latest_account_list = Gene.objects.all()[:90]
-    return render_to_response('genes.html', {'latest_account_list': latest_account_list})
+    fullGene = Gene.objects.all()[:90]
+    search = request.POST.get('search',None)
+    return render_to_response('genes.html', {'fullGene': fullGene,'search': search},context_instance=RequestContext(request))
 
 def selectedGene(request, geneSymbol):
     selectedGenes = Gene.objects.filter(officalSymbal = geneSymbol)
