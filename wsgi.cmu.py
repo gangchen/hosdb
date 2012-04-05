90cb143a38b840a9e327b3b03807959aaeb39e07
Gang Chen <chengangcs@gmail.com>
2012-04-05 15:44:30 +0800

    add wsgi.cmu.py for CMU server

---
+++ wsgi.cmu.py
@@ -0,0 +1,22 @@
+import os, sys
+import site
+
+ALLDIRS = ['/var/www/hosdb']
+
+prev_sys_path = list(sys.path)
+
+for directory in ALLDIRS:
+    site.addsitedir(directory)
+
+new_sys_path = [] 
+for item in list(sys.path): 
+    if item not in prev_sys_path: 
+        new_sys_path.append(item) 
+        sys.path.remove(item) 
+sys.path[:0] = new_sys_path
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
