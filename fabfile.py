63a82cc1809b7a31dc7a30850a3dfd5d81c872d2
Gang Chen <chengangcs@gmail.com>
2012-04-05 23:08:44 +0800

    minor changes

--- fabfile.py
+++ fabfile.py
@@ -3,4 +3,5 @@ from fabric.context_managers import cd
 def update_hosdb():
     with cd('/var/www/hosdb/'):
         run('git pull')
+        run('python manage.py syncdb')
         run('python manage.py collectstatic')
